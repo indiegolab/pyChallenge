d8dcf6e15b89b5eed74db8b718c279d2a1b1d9da
Always Floating <40168104+indiegolab@users.noreply.github.com>
2020-12-15 00:40:45 +0900

    python basic functions and modules

--- 1.py
+++ 1.py
@@ -4,3 +4,36 @@ c= "hello"
 d = None
 
 print(a,b,c,d)
+
+# list
+li1 = [1,2,3,4]
+len(li1)
+
+# 다양한 내장함수
+li1.append(5)
+print(4 in li1)
+
+# List, Tuple, Dict
+list1 = [1,2,3]
+tuple1 = (1,2,3)
+dict1 = { 
+    "name": "nico",
+    "age": 22,
+    "fav_food": ["kimchi", "rice"]}
+print(type(list1), type(tuple1), type(dict1))
+
+# 1.3 Built in Functions
+# 1.4 Creating Functions
+def say_hello():
+    print('hello')
+
+say_hello()
+
+# keyworded Arguments
+
+
+
+
+
+
+ 
